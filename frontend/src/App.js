bdf9452d1a8e4e7491f8117b36be63a58e81fd6a
SajanShrestha671 <shresthasajan13@gmail.com>
2023-12-31 19:57:20 +1100

    UI of Header and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-
+import { Container } from 'react-bootstrap';
+import Header from "./components/header";
+import Footer from "./components/footer"
 function App() {
   return (
     <>
-    <h1>
-      Hello Sajan...!!!
-    </h1>
+    <Header />
+    <Container>
+    <main className="py-3">
+      <h1>Hello Sajan...!!!</h1>
+    </main>
+    </Container>
+    <Footer />
     </>
   );
 }
