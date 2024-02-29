65b43846e349225b10ebff44242016dea8e8f7db
SajanShrestha671 <shresthasajan13@gmail.com>
2024-03-01 08:50:30 +1100

    Backend for login

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,8 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import bcrypt from 'bcrypt';
-import User from './model/userModel.js'; 
+import User from './model/userModel.js';
+import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
@@ -44,6 +45,39 @@ app.post('/register',  async (req, res) => {
     }
 });
 
+app.post("/login", async (request, response) => {
+    try {
+      const user = await User.findOne({ email: request.body.email });
+      if (!user) {
+        return response.status(404).send({ message: "Email not found" });
+      }
+        const passwordCheck = await bcrypt.compare(request.body.password, user.password);
+      if (!passwordCheck) {
+        return response.status(400).send({ message: "Passwords do not match" });
+      }
+  
+      const token = jwt.sign(
+        {
+          userId: user._id,
+          userEmail: user.email,
+        },
+        "RANDOM-TOKEN",
+        { expiresIn: "24h" }
+      );
+      response.status(200).send({
+        message: "Login Successful",
+        email: user.email,
+        token,
+      });
+  
+    } catch (error) {
+      response.status(500).send({
+        message: "An error occurred",
+        error: error.message,
+      });
+    }
+  });
+  
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
